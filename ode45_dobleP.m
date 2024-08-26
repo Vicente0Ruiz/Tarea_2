deeaea5327f5089fcc6b1192072ca0e8d8d18a13
Vicente0Ruiz <vicente.ruiz@alumnos.udg.mx>
2024-08-26 17:13:04 -0600

    ode45_dobleP.m

--- ode45_dobleP.m
+++ ode45_dobleP.m
@@ -1,13 +1,18 @@
-[t,x] = ode45(@DobleP,[0,10],[0 (35*pi)/180 0 0]);
+theta1   = 0;
+theta2   = (35*pi)/180;
+theta1_d = 0;
+theta2_d = 0;
+
+[t,x] = ode45(@DobleP,[0 10], [theta1 theta2 theta1_d theta2_d]);
+
 figure(1)
-plot(t,x(:,1));
+subplot(2,1,1)
+sgtitle("Pendulo"); 
+plot(t,x(:,1)) % First state: theta1
 grid on
-title("posicion de theta");
-xlabel("Tiempo");
-ylabel("Radianes");
-figure(2)
-plot(t,x(:,2));
+xlabel("Tiempo");ylabel("rad");legend('Theta1')
+
+subplot(2,1,2)
+plot(t,x(:,2),'r') % Second state: theta2
 grid on
-title("Posicion theta 2");
-xlabel("Tiempo");
-ylabel("Radianes");
+xlabel("Tiempo");ylabel("rad");legend('Theta2')
